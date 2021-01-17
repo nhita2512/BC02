27024924d8d78344e39bc79f9a4d4c4991c24a40
nhita2512 <yennhitale2@gmail.com>
2021-01-17 16:59:30 +0700

    Them files

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExampleLayout from "./ExampleLayout";
+import DemoDataBinding from "./databinding/DemoDataBinding";
+import Login from "./login";
+import Showroom from "./show-room-car";
+import ListAndKeys from "./list-and-keys";
+import Props from "./props";
+import LayoutChiaComponents from "./self-practice-component";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ExampleLayout /> */}
+      {/* <DemoDataBinding /> */}
+      {/* <Login /> */}
+      {/* <Showroom /> */}
+      {/* <ListAndKeys /> */}
+      {/* <Props /> */}
+      <LayoutChiaComponents />
+    </div>
+  );
+}
+
+export default App;
